1f98cefe18b1eb6b6acc79ceb1f467a0a327ca53
Michal Moudrý <moudry.michal@live.com>
2024-01-28 21:02:58 +0100

    Init

---
+++ ConsoleProject/FinalGpa.cs
@@ -0,0 +1,24 @@
+namespace ConsoleProject.FinalGpa;
+
+internal static class FinalGpa
+{
+    public static void RunProject()
+    {
+        var studentName = "Sophia Johnson";
+
+        var course1Name = "English 101";
+        var course2Name = "Algebra 101";
+        var course3Name = "Biology 101";
+        var course4Name = "Computer Science I";
+        var course5Name = "Psychology 101";
+
+        var course1Credit = 3;
+        var course2Credit = 3;
+        var course3Credit = 4;
+        var course4Credit = 4;
+        var course5Credit = 3;
+
+        Console.WriteLine($"Student name: {studentName}");
+        Console.WriteLine("Final GPA:");
+    }
+}
